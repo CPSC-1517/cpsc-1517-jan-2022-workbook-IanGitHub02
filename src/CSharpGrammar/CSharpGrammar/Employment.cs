caf62dae9389a8880130c55b9384ba5c0501a4ff
IanGitHub02 <bryan.paul.i.mendoza@gmail.com>
2022-01-13 09:58:40 -0700

    Test

---
+++ src/CSharpGrammar/CSharpGrammar/Employment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsole.Data
+{
+    public class Employment
+    {
+        private string _title;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Title is required piece of data.");
+                }
+
+                // Data is considered valid
+                _title = value;
+
+            }
+        }
+    }
+}
---
+++ src/CSharpGrammar/CSharpGrammar/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+DisplayString("Hello, World!");
+
+static void DisplayString(string text)
+{
+    Console.Write(text);
+}
